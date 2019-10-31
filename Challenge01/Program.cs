2773fad3145101402670ce694fec4b8e00ca73bf
ignadwi19 <ignadwi19@gmail.com>
2019-10-31 13:42:09 +0700

    2nd commit

--- Challenge01/Program.cs
+++ Challenge01/Program.cs
@@ -6,11 +6,9 @@ namespace YourName
     {
         static void Main(string[] args)
         {
-            // Get your name by using Console.ReadLine()
-            // Example output: "Hello, my name is Salamander.",
-            // depends on user input.
-
-            Console.WriteLine("Hello, my name is {0}.");
+            Console.WriteLine("Enter Your Name");
+            string Input = Console.ReadLine();
+            Console.WriteLine($"Hello, my name is {Input}.");
         }
     }
 }
